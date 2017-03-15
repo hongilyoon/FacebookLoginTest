6bc250f10cc605b5db22388fb38c10feacd69db9
윤홍일 <hiyoon@yunhong-il-ui-iMac.local>
2017-03-16 01:08:42 +0900

    페이스북 로그인 로직 추가

--- app/src/main/java/com/example/hiyoon/facebooklogintest/MainActivity.java
+++ app/src/main/java/com/example/hiyoon/facebooklogintest/MainActivity.java
@@ -22,7 +22,7 @@ import static android.R.attr.data;
 public class MainActivity extends AppCompatActivity {
 
     // CallbackManager
-    private CallbackManager callbackManager;
+    private CallbackManager callbackManager = CallbackManager.Factory.create();
 
     // Create
     @Override
@@ -30,38 +30,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // CallBackManager 생성
-        callbackManager = CallbackManager.Factory.create();
-
-        // 로그인 버튼 로직 추가
-        LoginButton btnLogin = (LoginButton) findViewById(R.id.btn_login);
-        btnLogin.setReadPermissions(Arrays.asList("public_profile", "email"));
-        btnLogin.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        LoginButton loginButton = (LoginButton) findViewById(R.id.btn_login);
+        loginButton.setReadPermissions("email");
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
-                    @Override
-                    public void onCompleted(JSONObject object, GraphResponse response) {
-                        Log.v("result",object.toString());
-                    }
-                });
-
-                Bundle parameters = new Bundle();
-                parameters.putString("fields", "id,name,email,gender,birthday");
-                graphRequest.setParameters(parameters);
-                graphRequest.executeAsync();
+                // App code
             }
 
             @Override
             public void onCancel() {
-
+                // App code
             }
-
 
             @Override
-            public void onError(FacebookException error) {
-                Log.e("LoginErr",error.toString());
+            public void onError(FacebookException exception) {
+                // App code
             }
-        });
+    });
     }
 }
